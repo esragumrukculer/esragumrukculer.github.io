66a5ade4d3fe1319f7c5596666c02191586c574e
esragumrukculer <gumre166@newschool.edu>
2018-04-25 21:16:43 -0400

    in class work

---
+++ js class quiz/main.js
@@ -0,0 +1,101 @@
+// 1
+
+var fullname ='Esra GUmrukculer';
+
+
+var x = 4;
+
+var myfavnumber = '3';
+
+'true';
+
+var nameletters =['e', 's', 'r', 'a'];
+
+var nameandsurname ="esra " + "gumrukculer";
+
+
+var equalto100 ='z';
+var x = 60;
+var y = 40;
+var z = x + y;
+
+
+// 2
+
+console.log('e','a');
+
+
+// 3
+
+var supercalifragilisticexpialidocious = ['s', 'u', 'p', 'e','r', 'c', 'a', 'l','i', 'f', 'r', 'a','g', 'i', 'l', 'i', 's','t', 'i', 'c', 'e','x', 'p', 'i', 'a','l', 'i', 'd', 'o', 'c', 'i', 'o','u', 's'];
+console.log(supercalifragilisticexpialidocious.length);
+
+
+// 4
+
+
+console.log (gumrukculer.length) == console.log (esra.length);
+
+
+
+// 5
+
+
+var studentsHere == ['Esra', 'Evelin', 'Prithi','Emily', 'Emil' ,'Kristen','Yangchen', 'Yaren', 'Jahyun', 'Seowon', 'Soyeon','Vincent', 'Vinny', 'Khe', 'Shuting'];
+
+console.log(studentsHere.length)=15;
+
+var allstudents == 15;
+
+if ( studentsHere == 15 ) {
+  console.log("the students are in class");
+} else if ( hour > 15 ) {
+  console.log("not everyone is here");
+} 
+if ( hour == 0 ) {
+
+
+// 6
+
+var nameletters = ['e', 's', 'r', 'a'];
+
+for ( var i = 0; i < nameletters.length; i++ ) {
+  console.log( nameletters[i] + '' + i );
+}
+
+// 7-8
+
+var date = new Date;
+var time = date.getHours();
+
+if ( time > 7 && time < 9:40 ) {
+  console.log("checked");
+} 
+else if ( date = monday ) {
+  console.log("hi monday");
+}
+else if ( date = tuesday ) {
+  console.log("hi tuesday");
+}else if ( date = wednsday ) {
+  console.log("hi wednsday");
+}else if ( date = thursday ) {
+  console.log("hi thursday");
+}else if ( date = friday ) {
+  console.log("hi friday");
+}else if ( date = saturday ) {
+  console.log("hi saturday");
+}
+ else {
+  console.log("sunday");
+}
+
+// 9
+
+var favFood =['avocado', 'mango', 'tomatos'];
+var favAnimals =['ocelot', 'bonoboMonkey', 'parot'];
+var favPlaces =['Izmir', 'Torino', 'Havana'];
+var favPokemon =['idk-1', 'idk-2', 'idk-3'];
+var favs = [favFood, favAnimals, favPlaces, favPokemon];
+
+// 10
+
---
+++ quick_prototyping/2/main.js
@@ -0,0 +1,22 @@
+
+var date = new Date();
+
+function updateTime() {
+  date = new Date();
+  $('#time-gmt').html( date.toGMTString() );
+  $('#time-seconds').html( date.getTime() );
+}
+
+$(function() {
+  updateTime();
+  setInterval(updateTime, 50);
+  
+  $('#month').html( date.getMonth() );
+  $('#day').html( date.getDate() );
+  $('#year').html( date.getUTCFullYear() );
+});
+
+
+$(function() {
+  // Your interactions go here
+});
